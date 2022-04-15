57c3ce7e38c25ab3c6ae164d92f480e94ad640f7
jeonginmo18 <cat10830@naver.com>
2022-04-15 11:30:49 +0900

    mnist폼 기본 완성

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from requests import request
 
 app = Flask(__name__)
 
@@ -6,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/mnist', methods=['GET', 'POST'])
+def mnist():
+    if request.method == 'GET':
+        return render_template('mnistform.html')
+    else:
+        pass
+
+
 if __name__ ==  '__main__':
     app.run(debug=True)
-    
